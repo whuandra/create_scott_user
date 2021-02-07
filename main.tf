8510e823775cdc47f0b526e896cd7f08ddc6eaa9
whuandra <winoto@hotmail.com>
2021-02-06 16:49:58 -0800

    First commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = "us-west-1"
+
+}
+
+locals {
+    tags = {
+        BuildBy = "terraform"
+        Name = "scott user"
+    }
+}
+
+resource "aws_iam_user" "scott" {
+    name = "scott"
+    tags = locals.tags
+}
